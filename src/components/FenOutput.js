0ecebd99dcaf57614cc43c65df116a48892aa44b
Vitor B Appolinario <vitorappolinario@gmail.com>
2020-08-13 12:57:33 -0300

    Refactors FenOutput component
    Better separation of behavior from component

--- src/components/FenOutput.js
+++ src/components/FenOutput.js
@@ -1,26 +1,21 @@
 import React from 'react';
 import Chessboard from 'chessboardjsx';
 
+function validateFEN(fenString) {
+    return fenString
+        .split(' ')[0] // only iterested in the first field of FEN string
+        .replace(/[a-zA-z]/g, '1') // replace pieces to 1
+        .replace(/\//g, '') // remove separators
+        .split('') // get each char
+        .map(c => parseInt(c)) // get int value of that char
+        .reduce((a,b) => a+b) === 64; // the sum should be 64 (board squares)
+}
+
 const FenOutput = (props) => {
-
-    const isValidFen = props.value.replace(/[a-zA-z]/g, '1')
-        .replace(/\//g, '')
-        .split(' ')[0]
-        .split('')
-        .map(c => parseInt(c))
-        .reduce((a,b) => a+b) === 64;
-
-    console.log('validation', isValidFen);
-
-    return (
-        <div>
-            <h3>Result</h3>
-            <p>
-                <span className={ isValidFen ? "" : "alert" }
-                    onClick={() => {navigator.clipboard.writeText(props.value)}}>
-                    {isValidFen ? "" : "Invalid Position : "}{props.value}
-                </span>
-            </p>
+    const isValidFen = validateFEN(props.value);
+    const message = isValidFen ? props.value : "Invalid Initial FEN String";
+    const cssClass = isValidFen ? undefined : "alert" ;
+    const board = !isValidFen ? "" :
             <div className="chessBoard">
                 <Chessboard
                     position={props.value}
@@ -28,7 +23,18 @@ const FenOutput = (props) => {
                     showNotation={false}
                     width={320}
                 />
-            </div>
+            </div>;
+
+    return (
+        <div>
+            <h3>Result</h3>
+            <p>
+                <span className={cssClass}
+                    onClick={() => {navigator.clipboard.writeText(props.value)}}>
+                    {message}
+                </span>
+            </p>
+            {board}
         </div>
     );
 };
